ef3dac0e6275ed99570867ce08432505ee5043cd
Edwin Cini <edwin.cini@hotmail.co.uk>
2020-01-09 14:42:36 +0100

    Some further tweaks to SVG image generation from LED data structure

--- data/status.js
+++ data/status.js
@@ -1,10 +1,21 @@
 window.onload = function() {
-  var lightStatus = [
-    { r: 255, g: 0, b: 0 },
-    { r: 0, g: 255, b: 0 },
-    { r: 0, g: 0, b: 255 },
-    { r: 128, g: 128, b: 128 }
-  ];
+  var lightStatus = {
+    width: 19,
+    height: 20,
+    whiteColor: { r: 251, g: 241, b: 224 }, //http://planetpixelemporium.com/tutorialpages/light.html
+    whiteLevel: 255,
+    values: firePattern(19, 10)
+  };
+
+  function firePattern(width, height) {
+    var values = [];
+
+    for (var pixel = 0; pixel < width * height; pixel++) {
+      values[pixel] = { r: pixel * 1.5, g: pixel * 1.2, b: pixel * 0.5 };
+    }
+
+    return values;
+  }
 
   function doApiGet(url, successCallback) {
     var xhr = new XMLHttpRequest();
@@ -39,8 +50,6 @@ window.onload = function() {
     var xmlns = "http://www.w3.org/2000/svg";
     var image = document.createElementNS(xmlns, "svg");
     image.setAttributeNS(null, "viewBox", "0 0 " + width + " " + height);
-    //image.setAttributeNS(null, "width", width);
-    //image.setAttributeNS(null, "height", height);
     image.style.display = "block";
 
     var filter = document.createElementNS(xmlns, "filter");
@@ -57,24 +66,37 @@ window.onload = function() {
     image.appendChild(filter);
     image.appendChild(g);
 
-    var cellWidth = width / 19;
-    var cellHeight = height / 20;
-
-    for (var x = 0; x < 19; x++) {
-      for (var y = 0; y < 20; y++) {
-        var rDelta = Math.floor(256 / 19);
-        var gDelta = Math.floor(256 / 10);
-        var bDelta = Math.floor(256 / 29);
-        var cs = lightStatus[(x * y + y) % lightStatus.length];
-        var cellColour = "rgb(" + cs.r + "," + cs.g + "," + cs.b + ")";
-
+    var cellWidth = width / lightStatus.width;
+    var cellHeight = height / lightStatus.height;
+    for (var y = 0; y < lightStatus.height; y++) {
+      if (y % 2 == 0) {
+        // white background illumination
         var cell = document.createElementNS(xmlns, "rect");
-        cell.setAttributeNS(null, "width", cellWidth);
+        var wc = lightStatus.whiteColor;
+        var wl = lightStatus.whiteLevel / 256;
+        var cellColour =
+          "rgb(" + wc.r * wl + "," + wc.g * wl + "," + wc.b * wl + ")";
+
+        cell.setAttributeNS(null, "width", width);
         cell.setAttributeNS(null, "height", cellHeight);
-        cell.setAttributeNS(null, "x", x * cellWidth);
+        cell.setAttributeNS(null, "x", 0);
         cell.setAttributeNS(null, "y", y * cellHeight);
         cell.setAttributeNS(null, "fill", cellColour);
         g.appendChild(cell);
+      } else {
+        // rgb mood illumination
+        for (var x = 0; x < lightStatus.width; x++) {
+          var cs = lightStatus.values[(x * y + y) % lightStatus.values.length];
+          var cellColour = "rgb(" + cs.r + "," + cs.g + "," + cs.b + ")";
+
+          var cell = document.createElementNS(xmlns, "rect");
+          cell.setAttributeNS(null, "width", cellWidth);
+          cell.setAttributeNS(null, "height", cellHeight);
+          cell.setAttributeNS(null, "x", x * cellWidth);
+          cell.setAttributeNS(null, "y", y * cellHeight);
+          cell.setAttributeNS(null, "fill", cellColour);
+          g.appendChild(cell);
+        }
       }
     }
 
